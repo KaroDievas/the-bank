3a0b62d147766b1d133a90c0ec491a0d42b1db00
KaroDievas <admin@l2topai.eu>
2019-06-18 20:16:57 +0300

    Initial version for symfony API

---
+++ src/Entity/Transaction.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TransactionRepository")
+ */
+class Transaction
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $user_id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $details;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $receiver_account;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $receiver_name;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=3)
+     */
+    private $amount;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=3)
+     */
+    private $fee_amount;
+
+    /**
+     * @ORM\Column(type="string", length=3)
+     */
+    private $currency;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getDetails(): ?string
+    {
+        return $this->details;
+    }
+
+    public function setDetails(string $details): self
+    {
+        $this->details = $details;
+
+        return $this;
+    }
+
+    public function getReceiverAccount(): ?string
+    {
+        return $this->receiver_account;
+    }
+
+    public function setReceiverAccount(string $receiver_account): self
+    {
+        $this->receiver_account = $receiver_account;
+
+        return $this;
+    }
+
+    public function getReceiverName(): ?string
+    {
+        return $this->receiver_name;
+    }
+
+    public function setReceiverName(string $receiver_name): self
+    {
+        $this->receiver_name = $receiver_name;
+
+        return $this;
+    }
+
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    public function setAmount($amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getFeeAmount()
+    {
+        return $this->fee_amount;
+    }
+
+    public function setFeeAmount($fee_amount): self
+    {
+        $this->fee_amount = $fee_amount;
+
+        return $this;
+    }
+
+    public function getCurrency(): ?string
+    {
+        return $this->currency;
+    }
+
+    public function setCurrency(string $currency): self
+    {
+        $this->currency = $currency;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20190618171332.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190618171332 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE transaction (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, details VARCHAR(255) NOT NULL, receiver_account VARCHAR(255) NOT NULL, receiver_name VARCHAR(255) NOT NULL, amount NUMERIC(10, 3) NOT NULL, status VARCHAR(15) NOT NULL, fee_amount NUMERIC(10, 3) NOT NULL, currency VARCHAR(3) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE transaction');
+    }
+}
