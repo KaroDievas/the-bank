4f755ec27c960112081fe4402b5ff027dde63d34
KaroDievas <admin@l2topai.eu>
2019-06-20 19:30:50 +0300

    Docker files and command skeleton

--- src/Command/TransactionProcessorCommand.php
+++ src/Command/TransactionProcessorCommand.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Command;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -9,18 +11,36 @@ class TransactionProcessorCommand extends Command
 {
     protected static $defaultName = 'app:process-transactions';
 
-    public function __construct($name = null)
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(EntityManagerInterface $em, LoggerInterface $logger)
     {
-        parent::__construct($name);
+        $this->logger = $logger;
+        $this->em = $em;
+        parent::__construct();
     }
 
     protected function configure()
     {
-        // ...
+        $this
+            ->setName('app:process-transactions')
+            ->setDescription('Transaction processor!');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-//        $em = $this->
+        $this->logger->info('Starting ');
+        while (true) {
+            break;
+        }
+        $this->logger->info('End');
     }
 }
