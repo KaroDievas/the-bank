7e332b6890f5abb9b5241b6c6b8693d71800604e
KaroDievas <admin@l2topai.eu>
2019-06-20 19:42:15 +0300

    Final transaction processor version

--- src/Command/TransactionProcessorCommand.php
+++ src/Command/TransactionProcessorCommand.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Command;
 
+use App\Entity\Transaction;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
@@ -35,11 +36,27 @@ class TransactionProcessorCommand extends Command
             ->setDescription('Transaction processor!');
     }
 
+    /**
+     * Processes pending and signed transactions
+     * There is 5 seconds sleep after process
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|void|null
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->logger->info('Starting ');
+        $this->logger->info('Starting');
         while (true) {
-            break;
+            $pendingSignedTransactions = $this->em->getRepository(Transaction::class)->getTransactionsByStatusSignedPending();
+            foreach ($pendingSignedTransactions as $transaction) {
+                $this->logger->info(sprintf('Processing transaction with id = %s', $transaction->getId()));
+                $transaction->setStatus('COMPLETED');
+                $this->em->persist($transaction);
+                $this->em->flush();
+                $this->logger->info(sprintf('Processed transaction with id = %s', $transaction->getId()));
+            }
+            sleep(5);
         }
         $this->logger->info('End');
     }
